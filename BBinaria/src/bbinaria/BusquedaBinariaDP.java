432e91e10cd1ece05785c3fa745ea62a6f93178e
jaredait <jaredampudia@gmail.com>
2021-04-14 18:01:52 -0500

    Agregar clase BusquedaBinariaDP con atributos y medotos vacios

---
+++ BBinaria/src/bbinaria/BusquedaBinariaDP.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bbinaria;
+import java.io.File;
+
+/**
+ *
+ * @author ASUS
+ */
+public class BusquedaBinariaDP {
+    // Atributos
+    //Private Ordenamiento ordenamiento = new Ordenamiento();
+    private File archivo;
+    private int[] numerosArchivo;
+    private int numeroBuscar;
+    
+    // Constructor
+    public BusquedaBinariaDP(){
+        
+    }
+    
+    // Metodos
+    public void guardarDP(){
+        
+    }
+    public void cargarArchivo(){
+        
+    }
+    public int[] guardarEnArray(){
+        
+    }
+    public boolean verificarNumero(int numero){
+        
+    }
+    public boolean verificarArchivo(){
+        
+    }
+    public int[] buscarNumero(){
+        
+    }
+    public int busquedaBinaria(){
+        
+    }
+    public boolean eliminarArchivo(){
+        
+    }
+    
+}
